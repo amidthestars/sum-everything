abd84665d0cb336a061b0d0aecb65795951dbd1e
pushuzhang <62913990+pushuzhang@users.noreply.github.com>
2022-01-24 08:27:41 -0800

    Hindu/Indian Time Dataset (#4)
    Reviewed by: Jess Fan <jessefan2002@gmail.com>

---
+++ data/gen-idt.py
@@ -0,0 +1,35 @@
+import os
+import io
+import csv
+import tqdm
+import gdown
+import random
+import subprocess
+from src.helpers import clean
+
+DATA_URL = "https://drive.google.com/uc?export=download&id=14-yvKIYucN9dEq5S9FcwHnvL0EHGgkFx"
+IN = "idt"
+OUT = "../datasets/idt"
+PREFIX = "idt"
+
+if not os.path.exists("hindu_indian_times_guardian_news.zip"):
+    gdown.download(DATA_URL, "hindu_indian_times_guardian_news.zip", quiet=False)
+if not os.path.exists(IN):
+    os.mkdir(IN)
+    subprocess.call(['unzip', '-o', "hindu_indian_times_guardian_news.zip", '-d', "../data/idt"])
+
+files = os.listdir(IN)
+splits = ["train", "validation"]
+try: os.makedirs(OUT)
+except FileExistsError: pass
+outputs={split:io.open(os.path.join(OUT,f"{PREFIX}.{split}"), mode="w", encoding="utf-8") for split in splits}
+
+for filename in tqdm.tqdm(files):
+    with io.open(os.path.join(IN, filename), mode="r", encoding = "ISO-8859-1") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            if row[0] == "author": continue
+            try: outputs[random.choices(splits, weights = [80, 20])[0]].write(f"{clean(row[4])}\t{clean(row[5])}\n")
+            except IndexError: continue # skip empty entries
+
+for output in outputs: outputs[output].close()
