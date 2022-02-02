5bff800470b79bb52773c1e975220ca5d8312678
Sebby11 <used4sebastianid@gmail.com>
2022-02-02 13:27:31 -0800

    Limiter to input files

--- sumWeb/static/js/files.js
+++ sumWeb/static/js/files.js
@@ -7,10 +7,28 @@ upload_file_buttton.addEventListener('click', () => {
 
 // Read file data
 file_input.addEventListener('change', function() {
+    // Show added file properties
+    console.log(file_input.files)
+
     var fr = new FileReader();
     fr.onload = function(){
-        // Create new article
-        setArticle(String(+ new Date()), fr.result);
+        console.log("Got file!");
+
+        // Make sure the # of sentences is > 20 & < 1000
+        // NOTE: Splitting by period causes problems in cases like "Analyst John L. Allen said."
+        const sentences = fr.result.split('.').map(function (line) {
+            return line.replace('\n', '');
+        })
+        console.log(sentences);
+
+        if (sentences.length < 20 || sentences.length > 1000) {
+            alertStr = "File cannot have less than 20 sentences or more than 1000. Current #: " + sentences.length.toString();
+            showAlert(article_alert, alertStr, "red", "fa-exclamation-triangle")
+        }
+        else{
+            // Create new article
+            setArticle(String(+ new Date()), fr.result);
+        }
     }
     fr.readAsText(this.files[0]);
 })
