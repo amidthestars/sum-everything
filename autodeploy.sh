2805bd49a4e0565247f4d7ff5080691a54abf641
JEF1056 <jess@bigdatafed.com>
2022-02-14 10:24:49 -0800

    update errors

--- autodeploy.sh
+++ autodeploy.sh
@@ -11,11 +11,7 @@ tmux new-session -d -n killall
 tmux send-keys -t killall "sudo fuser -k 443/tcp" Enter
 sleep 1
 tmux kill-session -t webserver
-tmux kill-session -t webhooks
 sleep 1
-tmux new-session -d -n webserver
+tmux new -s webserver -d
 tmux send-keys -t webserver "cd /home/ubuntu/" Enter
 tmux send-keys -t webserver "sudo gunicorn --worker-class eventlet -w 2 main:app --bind=0.0.0.0:443" Enter
-tmux new-session -d -n webhooks
-tmux send-keys -t webserver "cd /home/ubuntu/sum-everything" Enter
-tmux send-keys -t webserver "webhook -hooks autodeploy.json -verbose" Enter
