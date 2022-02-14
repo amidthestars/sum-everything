70927cd2b7190f9e05362f0e73d56804871446c2
JEF1056 <jess@bigdatafed.com>
2022-02-14 11:13:34 -0800

    use port 80 for now

--- autodeploy.sh
+++ autodeploy.sh
@@ -10,7 +10,7 @@ chmod +x autodeploy.sh
 sudo -H pip3 install -U -r requirements.txt
 
 tmux new -s killall -d
-tmux send-keys -t killall "sudo fuser -k 443/tcp" Enter
+tmux send-keys -t killall "sudo fuser -k 80/tcp" Enter
 sleep 1
 tmux kill-session -t killall
 tmux kill-session -t webserver
@@ -18,4 +18,4 @@ sleep 1
 
 tmux new -s webserver -d
 tmux send-keys -t webserver "cd /home/ubuntu/sum-everything" Enter
-tmux send-keys -t webserver "sudo gunicorn --worker-class eventlet -w 2 main:app --bind=0.0.0.0:443" Enter
+tmux send-keys -t webserver "sudo gunicorn --worker-class eventlet -w 2 main:app --bind=0.0.0.0:80" Enter
