329a70e64c106a3f33622ce43af4d6518876150b
JEF1056 <jess@bigdatafed.com>
2022-02-14 10:52:18 -0800

    update webhooks

--- startup.sh
+++ startup.sh
@@ -3,18 +3,19 @@
 cd /home/ubuntu
 sudo apt update
 sudo apt upgrade -y
+sudo apt autoremove -y
 
 tmux new -s killall -d
 tmux send-keys -t killall "sudo fuser -k 3000/tcp" Enter
 sleep 1
 tmux kill-session -t killall
 tmux kill-session -t tf-serving
-tmux kill-session -t webserver
+tmux kill-session -t webhook
 sleep 1
 
 tmux new -s tf-serving -d
 tmux send-keys -t tf-serving "cd /home/ubuntu/" Enter
 tmux send-keys -t tf-serving "./tensorflow_model_server --rest_api_port=3000 --model_config_file='gs://sum-exported/models.config' --model_config_file_poll_wait_seconds=60 --rest_api_timeout_in_ms=3000000" Enter
-tmux new -s webserver -d
-tmux send-keys -t webserver "cd /home/ubuntu/sum-everything" Enter
-tmux send-keys -t webserver "webhook -hooks autodeploy.json -verbose" Enter
+tmux new -s webhook -d
+tmux send-keys -t webhook "cd /home/ubuntu/sum-everything" Enter
+tmux send-keys -t webhook "webhook -hooks autodeploy.json -verbose" Enter
