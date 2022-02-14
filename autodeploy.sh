4b1d3985ab597c4b8771a68dc47eac10417a5f1e
JEF1056 <jess@bigdatafed.com>
2022-02-14 10:03:25 -0800

    push shell script fix

--- autodeploy.sh
+++ autodeploy.sh
@@ -1,1 +1,11 @@
-sudo gunicorn --worker-class eventlet -w 2 main:app --bind=0.0.0.0:443
+cd "/home/ubuntu"
+git pull
+chmod +x autodeploy.sh
+tmux new-session -d -n killall
+tmux send-keys -t killall "sudo fuser -k 443/tcp" Enter
+sleep 1
+tmux kill-server
+sleep 1
+tmux new-session -d -n webserver
+tmux send-keys -t tf-serving "cd /home/ubuntu/" Enter
+tmux send-keys -t tf-serving "sudo gunicorn --worker-class eventlet -w 2 main:app --bind=0.0.0.0:443" Enter
