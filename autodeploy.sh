ffb358d90abbd45d3aaf1f22801e7cc97abc0533
JEF1056 <jess@bigdatafed.com>
2022-02-14 10:28:58 -0800

    update again

--- autodeploy.sh
+++ autodeploy.sh
@@ -8,11 +8,13 @@ mv startup.sh /home/ubuntu/startup.sh
 chmod +x /home/ubuntu/startup.sh
 chmod +x autodeploy.sh
 pip3 install -U -r requrements.txt
-tmux new-session -d -n killall
+
+tmux new -s killall -d
 tmux send-keys -t killall "sudo fuser -k 443/tcp" Enter
 sleep 1
 tmux kill-session -t webserver
 sleep 1
+
 tmux new -s webserver -d
 tmux send-keys -t webserver "cd /home/ubuntu/" Enter
 tmux send-keys -t webserver "sudo gunicorn --worker-class eventlet -w 2 main:app --bind=0.0.0.0:443" Enter
--- startup.sh
+++ startup.sh
@@ -3,12 +3,15 @@
 cd /home/ubuntu
 sudo apt update
 sudo apt upgrade -y
-tmux new-session -d -n killall
+
+tmux new -s killall -d
 tmux send-keys -t killall "sudo fuser -k 3000/tcp" Enter
 sleep 1
+tmux kill-session -t killall
 tmux kill-session -t tf-serving
 tmux kill-session -t webserver
 sleep 1
+
 tmux new -s tf-serving -d
 tmux send-keys -t tf-serving "cd /home/ubuntu/" Enter
 tmux send-keys -t tf-serving "./tensorflow_model_server --rest_api_port=3000 --model_config_file='gs://sum-exported/models.config' --model_config_file_poll_wait_seconds=60 --rest_api_timeout_in_ms=3000000" Enter
