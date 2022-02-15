56d191aebfd494b6a6cb47ac39e21c42bf8a4fa8
JEF1056 <jessefan2002@gmail.com>
2022-02-15 11:32:14 -0800

    update eval

--- train/eval.py
+++ train/eval.py
@@ -30,6 +30,10 @@ parser.add_argument('-datasets', nargs='+', type=str, required=True,
                     help='Names of dataset(s) as defined in datasets.json')
 parser.add_argument('-model_size', type=str, default="small", choices=["small", "t5.1.1.small", "base", "large", "3B", "11B"],
                     help='Model size. Match model size used in training')
+parser.add_argument('-tpu', type=str, default=None,
+                    help='TPU ip address. None if using GPU. local if TPU is attached to the local instance.')
+parser.add_argument('-tpu_topology', type=str, default=None, choices=["v2-8","v3-8", None],
+                    help='TPU type. None if using GPU')
 parser.add_argument('-models_dir', type=str, default="models",
                     help='Where model checkpoints are stored. Start with gs:// to upload to google cloud platform')
 parser.add_argument('-in_len', type=int, default=2048,
@@ -40,10 +44,6 @@ parser.add_argument('-out_len', type=int, default=512,
 # There are already defaults for these values per model size. These are simply overrides.
 parser.add_argument('-batch_size', type=int, default=None,
                     help='Batches per step')
-parser.add_argument('-max_checkpoints', type=int, default=None,
-                    help='The maximum number of checkpoints to save at once')
-parser.add_argument('-storemode', type=str, default="gs", choices=["gs", "local"],
-                    help='Whee is the dataset and checkpoint saved? Local or google cloud storage.')
 parser.add_argument('-model_paralellism', type=int, default=None,
                     help='Contrary to data paralellism, model paralellism splits a model up into each accelerator, helping memory usage but reducing overall model efficiency.')
 args = parser.parse_args()
@@ -74,22 +74,9 @@ seqio.MixtureRegistry.add(
 MODEL_SIZE = args.model_size
 MODEL_DIR = os.path.join(args.models_dir, MODEL_SIZE)
 
-# Set parallelism and batch size to fit on v2-8 TPU (if possible).
-# Limit number of checkpoints to fit within 5GB (if possible).
-try:
-    model_parallelism, train_batch_size, keep_checkpoint_max = {
-        "small": (1, 512, 4),
-        "t5.1.1.small": (1, 512, 4),
-        "base": (2, 256, 2),
-        "large": (4, 128, 2),
-        "3B": (8, 16, 1),
-        "11B": (8, 4, 1)}[MODEL_SIZE]
-except:
-    model_parallelism, train_batch_size, keep_checkpoint_max=None,None,None
-    assert args.model_paralellism and args.batch_size and args.max_checkpoints, "Model not found in supported list. Cannot determine model paralellism, batch size, and number of checkpoints to keep automatically."
+model_paralellism, eval_batch_size = 8, 1
 if args.model_paralellism: model_paralellism=args.model_paralellism
-if args.batch_size: train_batch_size=args.batch_size
-if args.max_checkpoints: keep_checkpoint_max=args.max_checkpoints
+if args.batch_size: eval_batch_size=args.batch_size
 
 tf.io.gfile.makedirs(MODEL_DIR)
 tf.io.gfile.makedirs(os.path.join(MODEL_DIR, "validation_eval"))
@@ -97,10 +84,13 @@ tf.io.gfile.makedirs(os.path.join(MODEL_DIR, "validation_eval"))
 
 model = t5.models.MtfModel(
     model_dir=MODEL_DIR,
-    tpu=None,
-    model_parallelism=model_parallelism,
-    batch_size=train_batch_size,
-    iterations_per_loop=500,
+    tpu=args.tpu,
+    tpu_topology=args.tpu_topology,
+    model_parallelism=model_paralellism,
+    batch_size=eval_batch_size,
+    sequence_length={"inputs": args.in_len, "targets": args.out_len},
+    learning_rate_schedule=0.001,
+    iterations_per_loop=10,
 )
 
 model.eval(
