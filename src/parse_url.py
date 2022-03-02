db3efa0029f16454544bb2eaca3eb061cdc516d6
Sebby11 <used4sebastianid@gmail.com>
2022-03-01 21:24:20 -0800

    better parsing?

--- src/parse_url.py
+++ src/parse_url.py
@@ -1,7 +1,9 @@
 # TODO: Write unit tests
 import nltk
 import requests
+import urllib.request
 from bs4 import BeautifulSoup
+
 
 def parse_url(url):
     adStuff = ['Advertisement', "Supported by"]
@@ -21,28 +23,69 @@ def parse_url(url):
 
     soup = BeautifulSoup(page.text, "html.parser")
 
-    links = soup.find_all('p', attrs={'class': 'css-axufdj evys1bk0'})
+    if False:
+        links = soup.find_all('p', attrs={'class': 'css-axufdj evys1bk0'})
+    else:
+        links = soup.find_all('p')
 
-    article = ''
+        print("*** ALL LINKS ***")
+        print(links)
 
-    for e in links:
-        m = e.getText()
-
-        if m in adStuff:
-            continue
-
-        # If less, then add it
-        if len(article + m) <= 5000:
-            article = article + m
-        else:
-            sentences = nltk.tokenize.sent_tokenize(m)
-            for sent in sentences:
-                if len(article + ' ' + sent) <= 5000:
-                    article = article + ' ' + sent
+        # Collect class names (classes: dict of [class names](keys) & [text content](vals) )
+        classes = {}
+        for e in links:
+            if e.has_attr("class"):
+                txt = e.getText()
+                name = str(e["class"])
+                if name not in classes:
+                    classes[name] = txt
                 else:
-                    break
+                    classes[name] += ' ' + txt
+            else:
+                txt = e.getText()
+                if "classless" not in classes:
+                    classes["classless"] = txt
+                else:
+                    classes["classless"] += ' ' + txt
 
-            break
+        # for each class get number of sentences
+        numSentences = {}
+        mostSentences = -1
+        likelyArticleClass = ''
+        for className in classes.keys():
+            # Holds number of sentences as well as list of sentences in case they're used
+            numSentences[className] = [0, []]
+            sentences = nltk.tokenize.sent_tokenize(classes[className])
+            numSentences[className][0] += len(sentences)
+            numSentences[className][1] = sentences
 
-    message = {'article': article}
-    return message
+            if numSentences[className][0] > mostSentences:
+                mostSentences = numSentences[className][0]
+                likelyArticleClass = className
+
+        # print(numSentences[likelyArticleClass][1])
+        article = ''
+        for sent in numSentences[likelyArticleClass][1]:
+            if len(article + sent) <= 5000:
+                if len(article) == 0:
+                    article = article + sent
+                else:
+                    article = article + " " + sent
+            else:
+                # print(sent)
+                # print()
+                sentences = nltk.tokenize.sent_tokenize(sent)
+                # print(sentences)
+                for s in sentences:
+                    tmp = article + " " + s
+                    if len(tmp) <= 5000:
+                        article = tmp
+                    else:
+                        break
+                break
+
+        print("** ARTICLE **\n", article)
+        print(len(article))
+
+        message = {'article': article}
+        return message
